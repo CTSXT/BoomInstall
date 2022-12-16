02ea964f5f8efefad651a53ada6dc536280fa683
David Haralambiev <david@davidharalambiev.tech>
2022-12-16 17:01:49 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import "../scss/app.scss";
-const R = require("ramda");
+import * as R from "ramda"
+//const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 const arrayToPluck = [
