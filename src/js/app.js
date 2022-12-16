c3734ab9d6d4f1525b07e0d4240581bacf356489
David Haralambiev <david@davidharalambiev.tech>
2022-12-16 15:54:58 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,12 @@ import "../scss/app.scss";
 const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const ul = document.querySelector("ul");
-  const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+const arrayToPluck = [
+    { name: "John", class: "is-primary" },
+    { age: 23, class: "is-warning" },
+    { job: "programmer", class: "is-danger" },
+  ];
+  const articles = document.querySelectorAll("article");
+  const filteredArray = R.filter(, );
+  
 });
